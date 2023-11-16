89e782718d2804b85aae0ed040d86308ac9a923e
clair <clairhany2017@gmail.com>
2023-11-15 16:53:26 -0800

    path

---
+++ path_handler.h
@@ -0,0 +1,9 @@
+#ifndef PATH_HANDLER_H
+#define PATH_HANDLER_H
+
+void execute_command_with_path(char *command);
+char* get_path();
+void execute_command(char *input);
+char* read_input(void);
+
+#endif
